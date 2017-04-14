2d1c410351ba15f80793514d506955edb5ef1c17
tshepontlhokoa <tshepo.ntlhokoa@gmail.com>
2017-04-14 14:14:16 +0200

    initial

---
+++ code/after/gruntfile.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        ember_handlebars: {
+            compile: {
+                options: {
+                    processName: function (sourceFile) {                        
+                        return sourceFile.replace("js/views/", "")
+                                          .replace(".handlebars", "");
+                    }
+                },
+                files: {
+                    "js/views/indexview.template.js":
+                                    "js/views/indexview.handlebars",
+                    "js/views/applicationview.template.js":
+                                    "js/views/applicationview.handlebars"
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-ember-handlebars');
+    grunt.registerTask('default', ['ember_handlebars']);
+}
---
+++ code/after/readme.txt
@@ -0,0 +1,4 @@
+to ensure that all node modules are downloaded and installed execute the command:
+
+	npm install
+
